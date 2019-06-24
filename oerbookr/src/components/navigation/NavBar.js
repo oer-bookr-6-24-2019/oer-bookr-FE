3fac9875fdabf678241eb438da257c3c8b7dbc56
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-24 14:01:20 -0700

    When click logout, logs you out and pushes you to '/login'

--- oerbookr/src/components/navigation/NavBar.js
+++ oerbookr/src/components/navigation/NavBar.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
-export default function NavBar() {
+function NavBar({ history }) {
   const loggedIn = localStorage.getItem("token");
   return (
     <div className="nav_bar_container">
       <nav>
         <NavLink to="/">{loggedIn ? "Home" : null}</NavLink>
-        {loggedIn ? <NavLink className="logout_button" to="">Logout</NavLink> : <NavLink to="/">Log In</NavLink>}
+        {loggedIn ? (
+          <NavLink
+            className="logout_button"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("token");
+              history.push("/");
+            }}
+          >
+            Logout
+          </NavLink>
+        ) : (
+          <NavLink to="/">Log In</NavLink>
+        )}
       </nav>
     </div>
   );
 }
+
+export default withRouter(NavBar);
