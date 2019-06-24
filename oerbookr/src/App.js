afa160db553853a4def23dec0766c159d35a3420
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-24 13:08:19 -0700

    imported PrivateRoute component and used to protect routes

--- oerbookr/src/App.js
+++ oerbookr/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import LoginPage from './views/LoginPage';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import LoginPage from "./views/LoginPage";
+import HomePage from "./views/HomePage";
+import SingleBookPage from "./views/SingleBookPage";
+import AddReviewPage from "./views/AddReviewPage";
+import PrivateRoute from './components/PrivateRoutes/PrivateRoute'
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Switch>
-        <Route path='/login' component={LoginPage} />
+        <Route path="/login" component={LoginPage} />
+        <PrivateRoute path="/" exact component={HomePage} />
+        <PrivateRoute path="/book/:id" exact component={SingleBookPage} />
+        <PrivateRoute path="/add-review/:id" exact component={AddReviewPage} />
       </Switch>
     </div>
   );
