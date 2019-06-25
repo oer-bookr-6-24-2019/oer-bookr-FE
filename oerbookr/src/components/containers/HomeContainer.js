2a3136ec4a2267a2e54c1f37f78c705591b57a9f
Jose Montero Jr <josemariodev@gmail.com>
2019-06-25 18:06:52 -0500

    displaying books

---
+++ oerbookr/src/components/containers/HomeContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link, Route } from 'react-router-dom';
+import BookCard from '../cards/BookCard';
+
+export default class HomeContainer extends Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://sgs-lambda-bookr.herokuapp.com/books/books')
+      .then(res => this.setState({ books: res.data }))
+
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.books.map(book => {
+          return (
+            <BookCard book={book}/>            
+          );
+        })}
+      </div>
+    );
+  }
+}
