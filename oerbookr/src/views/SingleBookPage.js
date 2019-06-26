3fc54aa062b2390896f86d6f9b057ab48504cc99
Jose Montero Jr <josemariodev@gmail.com>
2019-06-26 15:03:36 -0500

    delete book functional

--- oerbookr/src/views/SingleBookPage.js
+++ oerbookr/src/views/SingleBookPage.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import ReviewCard from '../components/cards/ReviewCard';
+import Modal from 'react-responsive-modal';
 export default class SingleBookPage extends Component {
   state = {
     book: {},
+    fetched: false,
+    isOpen: false,
   };
 
   componentDidMount() {
@@ -13,9 +17,21 @@ export default class SingleBookPage extends Component {
           this.props.match.params.id
         }`,
       )
-      .then(res => this.setState({ book: res.data }))
+      .then(res => this.setState({ book: res.data, fetched: true }))
       .catch(err => console.log(err));
   }
+
+  deleteBookHandler = e => {
+    e.preventDefault();
+    axios
+      .delete(
+        `https://sgs-lambda-bookr.herokuapp.com/books/delete/${
+          this.props.match.params.id
+        }`,
+      )
+      .then(res => this.props.history.push('/'))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
@@ -24,10 +40,28 @@ export default class SingleBookPage extends Component {
         <h2>{this.state.book.author}</h2>
         <h2>{this.state.book.license}</h2>
         <h2>{this.state.book.publisher}</h2>
-        {/* {this.state.book.reviews.map(review => {} )} */}
-        <Link>
-          <button>Add a Review</button>
-        </Link>
+        {this.state.fetched &&
+          this.state.book.reviews.map(review => {
+            return <ReviewCard key={review.reviewid} review={review} />;
+          })}
+        <h2>
+          <Link to={`/add-review/${this.props.match.params.id}`}>
+            Add a Review
+          </Link>{' '}
+        </h2>
+        <h2>
+          <a onClick={() => this.setState({ isOpen: true })}>Delete Book</a>
+          <Modal
+            open={this.state.isOpen}
+            closeIconSize={25}
+            styles={{}}
+            onClose={() => this.setState({ isOpen: false })}
+          >
+            <p>Are you sure?</p>
+            <button onClick={this.deleteBookHandler}>Yes</button>
+            <button>No</button>
+          </Modal>
+        </h2>
       </div>
     );
   }
