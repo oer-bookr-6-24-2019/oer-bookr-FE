cb84841410a96aee6e0662744fdc07118d35729e
Jose Montero Jr <josemariodev@gmail.com>
2019-06-26 15:03:20 -0500

    review handler added

--- oerbookr/src/views/AddReviewPage.js
+++ oerbookr/src/views/AddReviewPage.js
@@ -1,42 +1,63 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import { reviewStructure } from "../dummy-data-structures/review-structure";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { reviewStructure } from '../dummy-data-structures/review-structure';
+import axios from 'axios';
 
 class AddReviewPage extends React.Component {
-  state = { review: "", rating: null, user: "" };
+  state = { review: '', rating: null, user: '' };
 
   changeHandler = e => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   setRating = ratingNumber => {
     this.setState({
-        rating: ratingNumber
-    })
-  }
+      rating: ratingNumber,
+    });
+  };
+
+  reviewSubmitHandler = e => {
+    e.preventDefault();
+    axios
+      .post(
+        `https://sgs-lambda-bookr.herokuapp.com/reviews/add/bybookid/${
+          this.props.match.params.id
+        }`,
+        this.state,
+      )
+      .then(res =>
+        this.props.history.push(`/book/${this.props.match.params.id}`),
+      )
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
-      <div className="review_container">
+      <div className='review_container'>
         <form>
           <label>Review</label>
           <textarea
             required
-            cols="75"
-            rows="6"
-            name="review"
+            cols='75'
+            rows='6'
+            name='review'
             onChange={this.changeHandler}
           />
           <label>Full Name</label>
-          <input name="user" onChange={this.changeHandler} required />
-          <div className="ratings_container">
-          {reviewStructure.map(ratingObj => {
-            return <i className="far fa-star fa-3x" onClick={() => this.setRating(ratingObj.rating)}/>;
-          })}
+          <input name='user' onChange={this.changeHandler} required />
+          <div className='ratings_container'>
+            {reviewStructure.map(ratingObj => {
+              return (
+                <i
+                  className='far fa-star fa-3x'
+                  onClick={() => this.setRating(ratingObj.rating)}
+                />
+              );
+            })}
           </div>
-          <button>Add Review</button>
+          <button onClick={this.reviewSubmitHandler}>Add Review</button>
         </form>
       </div>
     );
