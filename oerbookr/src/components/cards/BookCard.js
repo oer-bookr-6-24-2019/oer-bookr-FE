5f1ba20dff34baa7add101da0045b90842f6fdf1
Jose Montero Jr <josemariodev@gmail.com>
2019-06-27 19:57:21 -0500

    home page styling

--- oerbookr/src/components/cards/BookCard.js
+++ oerbookr/src/components/cards/BookCard.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/bookCardStyles.scss';
+import bookCover1 from '../../img/bookCover1.jpg';
 
 const BookCard = props => {
   return (
     <div className='bookCard_container'>
       <Link to={`/book/${props.book.bookid}`}>
-        <img src={props.book.imageurl} alt='book' />
-        <h1>Title: {props.book.booktitle}</h1>
-        <h2>Author: {props.book.author}</h2>
-        <h2>Publisher: {props.book.publisher}</h2>
+        <div className='book_img_container'>
+          <img src={bookCover1} alt='book cover' />
+        </div>
+        <div className='bookInfo_container'>
+          <h1>Title: {props.book.booktitle}</h1>
+          <h2>Author: {props.book.author}</h2>
+          <h2>Publisher: {props.book.publisher}</h2>
+        </div>{' '}
+        <p>Learn More...</p>
       </Link>
     </div>
   );
