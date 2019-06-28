8f1febab8a1aa20f45f309e2345203152962e465
Jose Montero Jr <josemariodev@gmail.com>
2019-06-27 23:28:14 -0500

    singlebookpage styled

--- oerbookr/src/views/SingleBookPage.js
+++ oerbookr/src/views/SingleBookPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import ReviewCard from '../components/cards/ReviewCard';
 import Modal from 'react-responsive-modal';
 import '../styles/_singleBookPageStyles.scss';
+import bookCover1 from '../img/bookCover1.jpg';
 export default class SingleBookPage extends Component {
   state = {
     book: {},
@@ -36,38 +37,44 @@ export default class SingleBookPage extends Component {
 
   render() {
     return (
-      <div>
+      <div className='single_book_container'>
+        <div className='book_img_container'>
+          <img src={bookCover1} alt='book cover' />
+        </div>
+
         <h1>{this.state.book.booktitle}</h1>
-        <h2>{this.state.book.author}</h2>
-        <h2>{this.state.book.license}</h2>
-        <h2>{this.state.book.publisher}</h2>
-        <h2>
-          {this.state.fetched &&
-            this.state.book.reviews.map(review => {
-              return <ReviewCard key={review.reviewid} review={review} />;
-            })}
-        </h2>
-        <h2>
-          <Link
-            className='link'
-            to={`/add-review/${this.props.match.params.id}`}
-          >
-            Add a Review
-          </Link>{' '}
-        </h2>
-        <h2>
-          <a onClick={() => this.setState({ isOpen: true })}>Delete Book</a>
-          <Modal
-            open={this.state.isOpen}
-            closeIconSize={25}
-            styles={{}}
-            onClose={() => this.setState({ isOpen: false })}
-          >
-            <p>Are you sure?</p>
-            <button onClick={this.deleteBookHandler}>Yes</button>
-            <button>No</button>
-          </Modal>
-        </h2>
+        <div className='single_book_info'>
+          <h2>Author: {this.state.book.author}</h2>
+          <h2>Publisher: {this.state.book.publisher}</h2>
+          <h2>
+            <h2>Reviews</h2>
+            {this.state.fetched &&
+              this.state.book.reviews.map(review => {
+                return <ReviewCard key={review.reviewid} review={review} />;
+              })}
+          </h2>
+          <h2>
+            <Link
+              className='link'
+              to={`/add-review/${this.props.match.params.id}`}
+            >
+              Add a Review
+            </Link>{' '}
+          </h2>
+          <h2>
+            <a onClick={() => this.setState({ isOpen: true })}>Delete Book</a>
+            <Modal
+              open={this.state.isOpen}
+              closeIconSize={25}
+              styles={{}}
+              onClose={() => this.setState({ isOpen: false })}
+            >
+              <p>Are you sure?</p>
+              <button onClick={this.deleteBookHandler}>Yes</button>
+              <button>No</button>
+            </Modal>
+          </h2>
+        </div>
       </div>
     );
   }
